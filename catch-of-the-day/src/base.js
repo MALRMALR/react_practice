e97d0667be7fdcba372bee2ae4265b1485372762
MALRMALR <michaelalross@gmail.com>
2017-05-19 15:36:19 -0700

    added firebase - data persist and localStorage implemented for order

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBpAHOMAfqE4uLV0v6mbGkmD28Xtxnvj4s",
+  authDomain: "catch-3b6ae.firebaseapp.com",
+  databaseURL: "https://catch-3b6ae.firebaseio.com"
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -12,9 +13,36 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind( this);
+
+    // get initial state
     this.state = {
       fishes: {},
       order: {}
+    }
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
+
+    // check if there is any order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+    if(localStorageRef) {
+      // update our app components order state
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
     }
   }
 
@@ -47,7 +75,7 @@ class App extends React.Component {
 
   }
   render() {
-    console.log(this);
+    // console.log(this);
     return (
       <div className="catch-of-the-day">
         <div className="menu">
@@ -61,7 +89,10 @@ class App extends React.Component {
             }
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+        params={this.props.params}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
